402e354fb4cf1c5e6ba5459c97ec5e243afcd5c1
WILLIAMS <Q5110442@live.tees.ac.uk>
2019-10-02 16:44:55 +0100

    finished the lmg function and imported a ops search libary

---
+++ src/imports/ops_search.clj
@@ -0,0 +1,71 @@
+(ns imports.ops_search
+  ^{:doc "A simple, partially optimised implementation of a breadth-first search mechanism for applying simple STRIPS-style operators."
+    :author "Simon Lynch"}
+  (:require [clojure.set :refer :all]
+            [clojure.pprint :refer :all]
+            [imports.matcher :refer :all]))
+
+
+(use '[imports.matcher :refer :all])
+(use '[imports.trace :refer :all])
+
+(defn finalise-results
+  "Reverses relevant parts of results."
+  [state-map]
+  (if-not (map? state-map)
+    state-map ;; Leave as it is.
+    {:state (:state state-map) ;; Else, reverse.
+     :path (reverse (:path state-map))
+     :cmds (reverse (:cmds state-map))
+     :txt (reverse (:txt state-map))}))
+
+(defn apply-op
+  ""
+  [op state world]
+  (mfor* [(:pre op) (seq (into world state))]
+         {:state (union (set (mout (:add op)))
+                        (difference state (set (mout (:del op)))))
+          :cmd (mout (:cmd op))
+          :txt (mout (:txt op))}))
+
+(defn apply-all
+  ""
+  [ops state world]
+  (reduce concat
+          (map (fn [x] (apply-op (x ops) state world))
+               (keys ops))))
+
+(defn update-state-map
+  ""
+  [old successor]
+  {:state (:state successor)
+   :path (cons (:state old) (:path old))
+   :cmds (cons (:cmd successor) (:cmds old))
+   :txt (cons (:txt successor) (:txt old))})
+
+(defn ops-search
+  ""
+  [start goal ops & {:keys [world debug]
+                     :or {debug false
+                          world #{}}}]
+  (let [start {:state (set start) :path () :cmds () :txt ()} ;; Using sets for state tuples.
+        world (set world)
+        goal? (fn [state] (mfind* [goal (into world (:state state))] state))]
+    (or (goal? start)
+        (finalise-results
+          (loop [waiting (list start)
+                 visited #{}]
+            (when debug (pprint (list 'waiting= waiting 'visited= visited)))
+            (if (empty? waiting) nil
+                                 (let [[next & waiting] waiting
+                                       {:keys [state path cmds]} next
+                                       visited? (partial contains? visited)]
+                                   (when debug (pprint (list 'next-state= state)))
+                                   (if (visited? state)
+                                     (recur waiting visited)
+                                     (let [succs (remove visited? (apply-all ops state world))
+                                           g (some goal? succs)]
+                                       (if g (update-state-map next g)
+                                             (recur
+                                               (concat waiting (map #(update-state-map next %) succs))
+                                               (conj visited state))))))))))))
--- src/projects/recap.clj
+++ src/projects/recap.clj
@@ -1,5 +1,9 @@
 (ns projects.recap)
 
+(use '[imports.matcher :refer :all])
+(use '[imports.trace :refer :all])
+(use '[imports.ops_search :refer :all])
+
 (defn nthItem [a b]
   (nth b a))
 
@@ -7,10 +11,27 @@
   (reduce + a ))
 
 (defn odd
-  ([a]
-   (odd a [] []))
-  ([a odds even]
-    (cond
-      (empty? a) (hash-map :odd odds :even even)
-      (odd? (first a)) (odd (rest a) (conj odds (first a)) even)
-      :else (odd (rest a) odds (conj even (first a))))))
+([a]
+  (odd a [] []))
+([a odds even]
+  (cond
+    (empty? a) (hash-map :odd odds :even even)
+    (odd? (first a)) (odd (rest a) (conj odds (first a)) even)
+    :else (odd (rest a) odds (conj even (first a))))))
+
+(defn lmg [x] (list (+ x 15) (* x 6) (- x 12)))
+
+(defn canDo?
+  ([start goal lmg]
+    (canDo? start goal lmg (conj [] start)))
+  ([start goal lmg been]
+   (cond (= start goal) (do (println start) (println "SUCCESS!") been)
+         (> start goal) false
+         (< start 0) false
+         (not (= (count been) (count (into [] (set been))))) false
+         :else
+         (do (println start)
+             (or (canDo? (first (lmg start)) goal lmg (conj been (first (lmg start))))
+                 (canDo? (second (lmg start)) goal lmg (conj been (second (lmg start))))
+                 (canDo? (nth (lmg start) 2) goal lmg (conj been (nth (lmg start) 2))))))))
+
