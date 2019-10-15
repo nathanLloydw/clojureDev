15afc484977494b6d8e3eb5a04a7dada26047b4f
Nathan Williams <nathan.w.newport@gmail.com>
2019-10-15 19:29:21 +0100

    add comment on how to run file

--- src/projects/projectFourA.clj
+++ src/projects/projectFourA.clj
@@ -7,13 +7,13 @@
 (use '[imports.breadth-search :refer :all])
 
 (def busRoutes
-  '{ :Newcastle ({:location "middlesbrough" :cost 5} {:location "darlington" :cost 4} {:location "durham" :cost 4})
-    :middlesbrough ({:location "redcar" :cost 2} {:location "hartlepool" :cost 2} {:location "northallerton" :cost 3} {:location "newcastle" :cost 5})
-    :darlington ({:location "newcaslte" :cost 6} {:location "middlesbrough" :cost 3} {:location "durham" :cost 4} {:location "northallerton" :cost 2} {:location "leeds" :cost 7})
+  '{ :Newcastle ({:location "middlesbrough" :cost 8} {:location "darlington" :cost 7} {:location "durham" :cost 6})
+    :middlesbrough ({:location "redcar" :cost 2} {:location "hartlepool" :cost 2} {:location "northallerton" :cost 3} {:location "newcastle" :cost 5} {:location "leeds" :cost 5})
+    :darlington ({:location "newcaslte" :cost 6} {:location "middlesbrough" :cost 3} {:location "durham" :cost 4} {:location "northallerton" :cost 2} {:location "leeds" :cost 17})
     :durham ({:location "darlington" :cost 3} {:location "newcastle" :cost 6})
     :northallerton ({:location "durham" :cost 4} {:location "darlington" :cost 5})
-    :leeds ({:location "manchester" :cost 8} {:location "york" :cost 6} {:location "wakefield" :cost 3})
-    :manchester ({:location "liverpool" :cost 9} {:location "warrington" :cost 3} {:location "bolton" :cost 4})
+    :leeds ({:location "manchester" :cost 18} {:location "york" :cost 6} {:location "wakefield" :cost 3})
+    :manchester ({:location "liverpool" :cost 19} {:location "warrington" :cost 3} {:location "bolton" :cost 4})
     :liverpool ( {:location "warrington" :cost 2} {:location "manchester" :cost 7} {:location "chester" :cost 4})
     })
 
@@ -29,7 +29,11 @@
       )))
 
 (defn a*lmgB [state]
-  (let [location (:location state)
+  (let [location (:state state)
         cost (:cost state)]
     (for [{loc :location cos :cost} (busRoutes (keyword location))]  (hash-map :cost (+ cos cost) :state loc))))
+
+;; (A*search {:state "Newcastle" :cost 0} "chester" a*lmgB)
+
+
 
